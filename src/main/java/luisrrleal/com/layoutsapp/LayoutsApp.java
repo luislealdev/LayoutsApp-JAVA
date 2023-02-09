40a84fa4fbb7cbf03ea160a38ac3bff0c0cbad69
Luis Leal <luisrrleal@gmail.com>
2023-02-09 10:33:51 -0600

    Add center section and a circle

--- src/main/java/luisrrleal/com/layoutsapp/LayoutsApp.java
+++ src/main/java/luisrrleal/com/layoutsapp/LayoutsApp.java
@@ -5,13 +5,13 @@ import javafx.collections.FXCollections;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -23,12 +23,9 @@ public class LayoutsApp extends Application {
         BorderPane root = new BorderPane();
         root.setTop(generateTopSection());
         root.setLeft(generateLeftSection());
+        root.setCenter(generateCenterSection());
         root.setRight(generateRightSection());
-
-        /*
-        root.setRight();
-        root.setBottom();
-         */
+        root.setBottom(generateBottomSection());
 
         Scene scene = new Scene(root, 320, 240);
         stage.setTitle("Hello!");
@@ -55,7 +52,20 @@ public class LayoutsApp extends Application {
         for(int i=1; i<=10;i++){
             vbox.getChildren().add(new ComboBox<>(FXCollections.observableArrayList(names)));
         }
+
         return vbox;
+    }
+
+    private StackPane generateCenterSection(){
+        StackPane stackPane = new StackPane();
+        stackPane.setAlignment(Pos.CENTER);
+
+        Circle circle = new Circle();
+        circle.setRadius(200);
+        circle.setFill(Color.BLACK);
+
+        stackPane.getChildren().add(circle);
+        return stackPane;
     }
 
     private VBox generateRightSection(){
@@ -72,6 +82,15 @@ public class LayoutsApp extends Application {
         }
 
         return vbox;
+    }
+
+    private HBox generateBottomSection(){
+        HBox hBox = new HBox(5);
+        for (int i = 1; i<=10;i++){
+            hBox.getChildren().add(new DatePicker());
+        }
+
+        return hBox;
     }
 
 
